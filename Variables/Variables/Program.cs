67a2e544cd7a00a8645118d03b8038b2fd34061b
Teosphalt <theophile.mariotte2204@gmail.com>
2023-05-22 23:42:41 +0200

    variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Théophile";
+            float age = 25.9f;
+
+            Console.WriteLine($"Bonjour, je m'appelle {name}, j'ai {age} ans");
+        }
+    }
+}
---
+++ Video1/Video1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//
+/*
+
+*/
+
+namespace Video1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Udemy!");
+        }
+    }
+}
